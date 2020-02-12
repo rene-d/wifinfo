09a0063937ae289ae0b20f51ee40d09005b0980d
rene-d <rene-d@users.noreply.github.com>
2020-02-12 13:00:57 +0100

    host/container

--- runtest.sh
+++ runtest.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
-cmake -DCODE_COVERAGE=ON -DCMAKE_BUILD_TYPE=Debug /tic
+if [ -f /.dockerenv ]; then
+    src_dir=/tic
+    cov_dir=/coverage
+else
+    src_dir=$(dirname $0)
+    cov_dir=./coverage
+fi
+
+cmake -DCODE_COVERAGE=ON -DCMAKE_BUILD_TYPE=Debug ${src_dir}
 make -j
 
 lcov --base-directory . --directory . --zerocounters -q
@@ -9,4 +17,4 @@ ctest --output-on-failure
 
 lcov --base-directory . --directory . -c -o tic.info
 lcov --remove tic.info "/usr*" -o tic.info
-genhtml -o /coverage -t "tic test coverage" --num-spaces 4 tic.info
+genhtml -o ${cov_dir} -t "tic test coverage" --num-spaces 4 tic.info
