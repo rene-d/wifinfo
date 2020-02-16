98bfc4cd68b7610ea08f455d46dd1a966b8e9171
rene-d <rene-d@users.noreply.github.com>
2020-02-16 16:21:49 +0100

    SSE plus robuste

--- dashboard/wifinfo_sse/influx.py
+++ dashboard/wifinfo_sse/influx.py
@@ -16,6 +16,8 @@ from influxdb import InfluxDBClient
 from sseclient import SSEClient
 import json
 import click
+import socket
+import sys
 
 
 # Tarifs réglementés EDF fin 2019
@@ -34,64 +36,42 @@ TVA_055 = 1.055  # TVA 5.5%
 TVA_20 = 1.20  # TVA 20%
 
 
-@click.command()
-@click.option(
-    "-freq", "frequency", help="fréquence d'écriture dans la base InfluxDb", type=click.IntRange(5, 3600), default=10,
-)
-@click.option(
-    "-module", "wifinfo_addr", help="adresse IP du module WifInfo", default="192.168.4.1",
-)
-@click.option("-host", "influxdb_addr", help="adresse IP du module WifInfo", default="localhost")
-def main(frequency, wifinfo_addr, influxdb_addr):
+class Sonde:
+    def __init__(self):
 
-    tarif_hc_ttc = (TARIF_HC + TCFE + CSPE) * TVA_20
-    tarif_hp_ttc = (TARIF_HP + TCFE + CSPE) * TVA_20
+        self.tarif_hc_ttc = (TARIF_HC + TCFE + CSPE) * TVA_20
+        self.tarif_hp_ttc = (TARIF_HP + TCFE + CSPE) * TVA_20
+        print("tarif_hc_ttc: ", self.tarif_hc_ttc)
+        print("tarif_hp_ttc: ", self.tarif_hp_ttc)
 
-    print("tarif_hc_ttc: ", tarif_hc_ttc)
-    print("tarif_hp_ttc: ", tarif_hp_ttc)
+    def connect(self, influxdb_addr):
 
-    # connexion à la base de données InfluxDB
-    client = InfluxDBClient(influxdb_addr, 8086)
-    database = "teleinfo"
+        # connexion à la base de données InfluxDB
+        self.client = InfluxDBClient(influxdb_addr, 8086)
+        database = "teleinfo"
+        while True:
+            try:
+                if not {"name": database} in self.client.get_list_database():
+                    # client.create_database(database)
+                    self.client.query(
+                        f'CREATE DATABASE "{database}" WITH DURATION 30d REPLICATION 1 NAME "month"', method="POST"
+                    )
+                self.client.switch_database(database)
+            except requests.exceptions.ConnectionError:
+                logging.info("InfluxDB is not reachable. Waiting 5 seconds to retry.")
+                time.sleep(5)
+            else:
+                break
 
-    while True:
-        try:
-            if not {"name": database} in client.get_list_database():
-                # client.create_database(database)
-                client.query(
-                    f'CREATE DATABASE "{database}" WITH DURATION 30d REPLICATION 1 NAME "month"', method="POST"
-                )
-            client.switch_database(database)
-        except requests.exceptions.ConnectionError:
-            logging.info("InfluxDB is not reachable. Waiting 5 seconds to retry.")
-            time.sleep(5)
-        else:
-            break
-
-    # source SSE
-    messages = SSEClient(f"http://{wifinfo_addr}/sse/tinfo.json")
-
-    last_write = 0
-    for msg in messages:
-
-        now = int(time.time() / frequency)
-        if now == last_write:
-            print(f"\033[2m{msg}\033[0m")
-            continue
-
-        print(msg)
-
-        last_write = now
-
-        data = json.loads(msg.data)
+    def insert_data(self, data):
         points = [
             {
                 "measurement": "conso",
                 "tags": {"host": data["ADCO"]},
                 "time": data["timestamp"],
                 "fields": {
-                    "COSTHC": round(data["HCHC"] / 1000 * tarif_hc_ttc, 4),
-                    "COSTHP": round(data["HCHP"] / 1000 * tarif_hp_ttc, 4),
+                    "COSTHC": round(data["HCHC"] / 1000 * self.tarif_hc_ttc, 4),
+                    "COSTHP": round(data["HCHP"] / 1000 * self.tarif_hp_ttc, 4),
                     "HCHC": data["HCHC"],
                     "HCHP": data["HCHP"],
                     "PAPP": data["PAPP"],
@@ -101,7 +81,69 @@ def main(frequency, wifinfo_addr, influxdb_addr):
             }
         ]
         # print(json.dumps(points, separators=(",", ":")))
-        client.write_points(points)
+        self.client.write_points(points)
+
+    def sse(self, frequency, wifinfo_addr):
+        while True:
+            try:
+                # source SSE
+                messages = SSEClient(f"http://{wifinfo_addr}/tic", timeout=31)
+
+                last_write = 0
+                for msg in messages:
+
+                    sys.stdout.flush()
+
+                    now = int(time.time() / frequency)
+                    if now == last_write:
+                        # trop fréquent: on ignore le message
+                        print(f"\033[2m{msg}\033[0m")
+                        continue
+
+                    print(msg)
+                    last_write = now
+                    self.insert_data(json.loads(msg.data))
+
+            except (socket.timeout, requests.exceptions.ConnectionError) as exception:
+                print(exception)
+                print("retry in 10s")
+                time.sleep(10)
+
+    def req(self, frequency, wifinfo_addr):
+        last_timestamp = None
+
+        while True:
+            rep = requests.get(f"http://{wifinfo_addr}/json", timeout=5)
+            if rep.status_code == 200:
+                data = rep.json()
+                if data["timestamp"] != last_timestamp:
+                    print(rep.content)
+                    last_timestamp = data["timestamp"]
+                    self.insert_data(data)
+            else:
+                print("HTTP code", rep.status_code)
+
+            time.sleep(frequency)
+
+
+@click.command()
+@click.option(
+    "-freq", "frequency", envvar="FREQUENCY", help="fréquence d'écriture dans la base InfluxDb", type=click.IntRange(5, 3600), default=10,
+)
+@click.option(
+    "-module", "wifinfo_addr", envvar="WIFINFO", help="adresse IP du module WifInfo", default="192.168.4.1",
+)
+@click.option("-host", "influxdb_addr", help="adresse IP du module WifInfo", default="influxdb")
+def main(frequency, wifinfo_addr, influxdb_addr):
+
+    sonde = Sonde()
+
+    sonde.connect(influxdb_addr)
+
+    if frequency >= 30:
+        sonde.req(frequency, wifinfo_addr)
+    else:
+        sonde.sse(frequency, wifinfo_addr)
 
 
 if __name__ == "__main__":
--- tools/sse.py
+++ tools/sse.py
@@ -4,14 +4,26 @@
 
 from sseclient import SSEClient
 import click
+import socket
+import requests
+import time
 
 
 @click.command(context_settings={"help_option_names": ["-h", "--help"]})
 @click.argument("server", default="192.168.4.1")
 def main(server):
-    messages = SSEClient(f"http://{server}/tic")
-    for msg in messages:
-        print(msg)
+
+    while True:
+        try:
+            messages = SSEClient(f"http://{server}/tic", timeout=10)
+            for msg in messages:
+                print(msg)
+
+        except (socket.timeout, requests.exceptions.ConnectionError) as exception:
+            print(exception)
+            for i in range(10, 0, -1):
+                print(f"retry in {i}s")
+                time.sleep(1)
 
 
 if __name__ == "__main__":
